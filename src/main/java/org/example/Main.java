7e5c25075df977f3b3ed8165bbabb483a4cf7b1a
bairuoyu <1740557728@qq.com>
2023-09-29 16:10:40 +0800

    76 最小覆盖子串

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
         Solution solution = new Solution();
         String s = "aaaaaaaaaaaabbbbbcdd";
         String t = "abcdd";
-        System.out.print("result :"+solution.myMinWindow(s, t));
+        System.out.print("result :"+solution.minWindow(s, t));
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -6,38 +6,42 @@ import com.alibaba.fastjson2.JSON;
 
 public class Solution {
 
-    public String myMinWindow(String s, String t) {
-        Map<Character,Integer> counts = new HashMap<>();
-        String ans = s;
-        for(char c:t.toCharArray()){
+    /**
+     * 76 最小覆盖子串
+     * @param s
+     * @param t
+     * @return
+     */
+    public String minWindow(String s, String t) {
+        Map<Character,Integer> counts = new HashMap<Character,Integer>();
+        for(char c : t.toCharArray()){
             counts.put(c,counts.getOrDefault(c,0)+1);
         }
-        int length = t.length();
-        for(int start = -1, end = 0; end < s.length(); end++){
-            char c = s.charAt(end);
-            if( counts.containsKey(c) ){
-                if( start == -1 ){
-                    start = end;
-                }
-                counts.put(c,counts.get(c)-1);
-                length--;
-                while( start <= end && counts.get(c) < 0 ){
-                    if(counts.containsKey(s.charAt(start))){
-                        counts.put(s.charAt(start),counts.get(s.charAt(start))+1);
-                        length++;
-                    }
-                    start++;
-                }
-                while( start <= end && !counts.containsKey(s.charAt(start) )){
-                    start++;
-                }
-                if( length == 0 && end - start + 1 <= ans.length()){
-                    System.out.print(s.substring(start,end+1)+"\n");
-                    ans = s.substring(start,end+1);
+        int size = t.length();
+        int length = Integer.MAX_VALUE;
+        String ans = "";
+        for(int start = 0, end = 0; end < s.length(); end++){
+            if( counts.containsKey(s.charAt(end))){
+                counts.put(s.charAt(end),counts.get(s.charAt(end))-1);
+                if(counts.get(s.charAt(end)) >= 0){
+                    size--;
                 }
             }
+            while( start <= end && size == 0 ){
+                if( end - start + 1 < length){
+                    length = end - start + 1;
+                    ans = s.substring(start,end+1);
+                }
+                if( counts.containsKey(s.charAt(start))){
+                    counts.put(s.charAt(start),counts.get(s.charAt(start))+1);
+                    if(counts.get(s.charAt(start)) > 0){
+                        size++;
+                    }
+                }
+                start++;
+            }
         }
-        return ans == s ? "" : ans;
+        return ans;
     }
 
 }
